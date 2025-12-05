3a9dc56403b34deebd795139c0e4133b84385876
John Evans <djjevans+github@googlemail.com>
2025-12-05 15:43:45 +0000

    Day 4

---
+++ day-4/4b.py
@@ -0,0 +1,63 @@
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+input_file = os.path.join(here, "input.txt")
+
+f = open(input_file, "r")
+
+layout = []
+
+def check_cell(row,col,row_length,col_length):
+    if row < 0:
+        return 0
+    if row >= row_length:
+        return 0
+    if col < 0: 
+        return 0
+    if col >= col_length:
+        return 0
+    if layout[row][col] == "@" or layout[row][col] == "&":
+        return 1
+    return 0
+
+for line in f:
+    layout.append(list(line.strip()))
+
+row_len = len(layout)
+col_len = len(layout[0])
+
+total_removed = 0
+
+while True:
+    for row_idx in range(row_len):
+        for col_idx in range(col_len):
+
+            if layout[row_idx][col_idx] != "@":
+                continue
+
+            # Check all 8 around this
+            count = 0 
+            count += check_cell(row_idx-1,col_idx-1,row_len,col_len)
+            count += check_cell(row_idx,col_idx-1,row_len,col_len)
+            count += check_cell(row_idx+1,col_idx-1,row_len,col_len)
+            count += check_cell(row_idx-1,col_idx,row_len,col_len)
+            count += check_cell(row_idx+1,col_idx,row_len,col_len)
+            count += check_cell(row_idx-1,col_idx+1,row_len,col_len)
+            count += check_cell(row_idx,col_idx+1,row_len,col_len)
+            count += check_cell(row_idx+1,col_idx+1,row_len,col_len)
+            if count <4:
+                layout[row_idx][col_idx] = "&"
+                layout[row_idx]
+
+    removed_this_time = 0
+    for row_idx in range(row_len):
+        for col_idx in range(col_len):
+                if layout[row_idx][col_idx] == "&":
+                    removed_this_time += 1
+                    layout[row_idx][col_idx] = "."
+    total_removed += removed_this_time
+    if removed_this_time == 0:
+        break
+
+
+print(total_removed)
